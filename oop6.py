52dd02f7aaaabcc25116aa549f92ed476f487b43
Md Naimur Rahman <rahman23105101275@diu.edu.bd>
2025-07-20 23:00:31 +0600

    Problem 6 is done

---
+++ oop6.py
@@ -0,0 +1,27 @@
+"""OOP Exercise 6: Class Inheritance
+Given:
+
+Create a Bus child class that inherits from the Vehicle class. The default fare charge for any vehicle is its seating capacity multiplied by 100 (seating capacity * 100).
+
+If the vehicle is a Bus instance, we need to add an extra 10% to the full fare as a maintenance charge. Therefore, the total fare for a Bus instance will be the final amount, calculated as total fare plus 10% of the total fare. (final amount = total fare + 10% of the total fare.)
+
+Note: The bus seating capacity is 50, so the final fare amount should be 5500."""
+
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+    def fare(self):
+        return self.capacity * 100
+
+class Bus(Vehicle):
+    
+    def fare(self):
+        total_fare=super().fare()
+        final_amount=total_fare+(total_fare*10)/100
+        return final_amount
+
+School_bus = Bus("School Volvo", 12, 50)
+print("Total Bus fare is:", School_bus.fare())
